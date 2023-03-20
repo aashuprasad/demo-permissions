0f7c149186017d1d4c7ed425e2295b6fb3ab6e59
Ashu Prasad <aashuprasad@live.com>
2023-03-20 17:49:31 +0530

    init commit

---
+++ app/src/main/java/com/example/permissions/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.permissions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun hasWriteExternalStoragePermission() =
+        ActivityCompat.checkSelfPermission()
+}
